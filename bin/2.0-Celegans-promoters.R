5706b254d3691a41e2fe9b1210926c2725f2ee40
Amir Levine <levamir@gmail.com>
2018-10-24 15:48:16 +0300

    Fetch C. elegans transcription factors

--- bin/2.0-Celegans-promoters.R
+++ bin/2.0-Celegans-promoters.R
@@ -1,32 +1,40 @@
-# Obtain promoters of C. elegans genes-----------------------------------------
-
-# Load bioconductor and packages-----------------------------------------------
+#+ Load bioconductor and packages ----
 source("https://bioconductor.org/biocLite.R")
-if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
-if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} 
-# Ensembl biomaRt to obtain C. elegans sequences.
+if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} # Ensembl biomaRt to obtain C. elegans sequences.
 
-# Select a BioMart database and celegans dataset-------------------------------
-ensembl = useMart("ENSEMBL_MART_ENSEMBL",dataset="celegans_gene_ensembl")
-# Shortcut command designed from the following:
 
-## listMarts() # check which BioMart web services are available
-## ensembl=useMart("ENSEMBL_MART_ENSEMBL") # choose to query the Ensembl BioMart database
-## listDatasets(ensembl) # look at which datasets are available in the selected BioMart
-## ensembl = useDataset("celegans_gene_ensembl",mart=ensembl) # choose to use the celegans dataset
+#'Select a BioMart database and celegans dataset. ----
+#'Shortcut command designed from the following:
+# listMarts() # check which BioMart web services are available
+# ensembl=useMart("ENSEMBL_MART_ENSEMBL") # choose to query the Ensembl BioMart database
+# listDatasets(ensembl) # look at which datasets are available in the selected BioMart
+# ensembl = useDataset("celegans_gene_ensembl",mart=ensembl) # choose to use the celegans dataset
+#+ Select a BioMart database and celegans dataset
+ensembl = biomaRt::useMart("ENSEMBL_MART_ENSEMBL",dataset="celegans_gene_ensembl")
 
-# Obtain gene IDs to all protein coding genes in the C. elegans biomart--------
+#' Obtain gene IDs to all protein coding genes in the C. elegans biomart ----
+#' Shortcut command designed from the following:
+# listFilters(ensembl) # browse filter options
+# attributes = listAttributes(ensembl) # values we are interested in to retrieve
+# attributePages(ensembl) # browse attributes categories
+# attributes[attributes$page == 'sequences',] # browse attributes for sequences
 mRNA.ids <- getBM( # gene ids to all protein coding genes in celegans biomart
-  attributes = c("ensembl_gene_id","external_gene_name"), 
+  attributes = c("ensembl_gene_id","external_gene_name","go_id"), 
   filters = "biotype", # focus on protein-coding genes as only these are identified by CEL-seq
   values = "protein_coding", 
   mart = ensembl)
-# Shortcut command designed from the following:
 
-# listFilters(ensembl) # browse filter options
-# attributes = listAttributes(ensembl) # values we are interested in to retrieve
-# attributePages() # browse attributes categories
-# attributes[attributes$page == 'sequences',] # browse attributes for sequences
+save(ensembl,mRNA.ids, file = "data/celegans-biomart-database-mRNAs.Rdata")
+
+
+# Obtain promoters of C. elegans genes-----------------------------------------
+
+#+ Load bioconductor and packages----------------------------------------------
+source("https://bioconductor.org/biocLite.R")
+if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
+if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} # Ensembl biomaRt to obtain C. elegans sequences.
+
+load("data/celegans-biomart-database-mRNAs.Rdata")
 
 # Define the promoter region, relative to transcription start-site (TSS)-------
 # 500 bp upstream, and 100 bp downstream to TSS. 
