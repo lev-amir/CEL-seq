52cf85922da050e061c99928126226a6a5ae84f0
Amir Levine <levamir@gmail.com>
2018-10-25 19:23:13 +0300

    Pushed aside all the analyses that checked for enrichemnt differentially per strain

--- bin/2.9-TFs-overlapping-DEG-heatmap.R
+++ bin/2.9-TFs-overlapping-DEG-heatmap.R
@@ -1,31 +1,40 @@
-#+ Load packages and previously generated data of transcription factors enrichment in each DEG set. ----
-source("https://bioconductor.org/biocLite.R")
-if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
-if (!require(PWMEnrich)) {install.packages("PWMEnrich"); library(PWMEnrich)}
-if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} 
-load("experiments/2018-09-02-TFs-motifs/data/TFs-enriched-DEG.Rdata")
-#' Load previosly obtained C. elegansensembl database
-load("data/celegans-biomart-database-mRNAs.Rdata")
 
-#' Fetch C. elegnas transcription factors based on GO terms.
-# GO term accession   GO term name
-# GO:0003700          DNA-binding transcription factor activity
-# GO:0006355          regulation of transcription, DNA-templated
-TFs <- getBM( # gene ids to all protein coding genes in celegans biomart
-  attributes = c("ensembl_gene_id","external_gene_name"), 
-  filters = c("biotype","go"), 
-  values = list(biotype = "protein_coding",
-                go = c("GO:0006355","GO:0003700")), 
-  mart = ensembl)
+if (!require(PWMEnrich)) {install.packages("PWMEnrich"); library(PWMEnrich)}
+load("experiments/2018-09-02-TFs-motifs/data/TFs-enriched-DEG.Rdata")
+
 
 #' Create p-values matrix. Obtain the p-values of motifs per sequence.
 #' - rows correspond to the different input sequences and the columns correspond to motifs.
 #' **sequence.nobg** contains the raw affinity scores.
 #' **sequence.bg** contains the corresponding P-values.
 pvals.dn <- res.dn$sequence.bg
-pvals.up <- res.dn$sequence.bg
+pvals.up <- res.up$sequence.bg
+pvals <- rbind(pvals.dn,pvals.up) # Combine both up- and down-regulated genes
+colnames(pvals) <- toupper(colnames(pvals)) # Change Transcription factor names to UPPER-CASE
+
+#' Grouping variables
+p <- data.frame(pvals = c(rownames(pvals.dn),rownames(pvals.up)),
+                direction = c(rep("dn",length(rownames(pvals.dn))),rep("up",length(rownames(pvals.up)))))
 
 
+#' Hierarchichal clustering the transcription factors based on their p-value score 
+#' to the genes that are differentially regulated by anc-1 RNAi.
+col_dend = hclust(dist(t(pvals)))
+
+#' Package to color cluster trees
+if (!require(dendextend)) {install.packages("dendextend"); library("dendextend")}
+
+#' based on (Heatmap - Static and Interactive: Absolute Guide)[http://www.sthda.com/english/articles/28-hierarchical-clustering-essentials/93-heatmap-static-and-interactive-absolute-guide/] 
+Heatmap(pvals,
+        name = "Motif presence (p-value)", # title of legend
+        split = p, # split by a vector specifying rowgroups
+        row_title = "Genes regulated by anc-1 RNAi",
+        column_names_side = "top",
+        show_row_names = FALSE,
+        show_row_dend = FALSE,
+        row_names_gp = gpar(fontsize = 6), # Text size for row names
+        column_names_gp = gpar(fontsize = 6), # Text size for column names
+        cluster_columns = color_branches(col_dend, k = 3))
 
 
 #' load the 
