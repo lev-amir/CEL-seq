93858ae7ae1a606d0c50d57ac6ca367586301ef5
Amir Levine <levamir@gmail.com>
2018-11-21 12:10:42 +0200

    Splitting the script that obtains C. elegans mRNA promotes into two

---
+++ bin/1.9-Celegans-mRNA-BioMaRt.R
@@ -0,0 +1,27 @@
+#+ Load bioconductor and packages ----
+source("https://bioconductor.org/biocLite.R")
+if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} # Ensembl biomaRt to obtain C. elegans sequences.
+if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
+
+#'Select a BioMart database and celegans dataset. ----
+#'Shortcut command designed from the following:
+# listMarts() # check which BioMart web services are available
+# ensembl=useMart("ENSEMBL_MART_ENSEMBL") # choose to query the Ensembl BioMart database
+# listDatasets(ensembl) # look at which datasets are available in the selected BioMart
+# ensembl = useDataset("celegans_gene_ensembl",mart=ensembl) # choose to use the celegans dataset
+#+ Select a BioMart database and celegans dataset
+ensembl = biomaRt::useMart("ENSEMBL_MART_ENSEMBL",dataset="celegans_gene_ensembl")
+
+#' Obtain gene IDs to all protein coding genes in the C. elegans biomart ----
+#' Shortcut command designed from the following:
+# listFilters(ensembl) # browse filter options
+# attributes = listAttributes(ensembl) # values we are interested in to retrieve
+# attributePages(ensembl) # browse attributes categories
+# attributes[attributes$page == 'sequences',] # browse attributes for sequences
+mRNA.ids <- getBM( # gene ids to all protein coding genes in celegans biomart
+  attributes = c("ensembl_gene_id","external_gene_name","go_id"), 
+  filters = "biotype", # focus on protein-coding genes as only these are identified by CEL-seq
+  values = "protein_coding", 
+  mart = ensembl)
+
+save(ensembl,mRNA.ids, file = "data/celegans-biomart-database-mRNAs.Rdata")
--- bin/2.0-Celegans-promoters-BioMaRt.R
+++ bin/2.0-Celegans-promoters-BioMaRt.R
@@ -1,39 +1,9 @@
 #+ Load bioconductor and packages ----
 source("https://bioconductor.org/biocLite.R")
 if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} # Ensembl biomaRt to obtain C. elegans sequences.
-
-
-#'Select a BioMart database and celegans dataset. ----
-#'Shortcut command designed from the following:
-# listMarts() # check which BioMart web services are available
-# ensembl=useMart("ENSEMBL_MART_ENSEMBL") # choose to query the Ensembl BioMart database
-# listDatasets(ensembl) # look at which datasets are available in the selected BioMart
-# ensembl = useDataset("celegans_gene_ensembl",mart=ensembl) # choose to use the celegans dataset
-#+ Select a BioMart database and celegans dataset
-ensembl = biomaRt::useMart("ENSEMBL_MART_ENSEMBL",dataset="celegans_gene_ensembl")
-
-#' Obtain gene IDs to all protein coding genes in the C. elegans biomart ----
-#' Shortcut command designed from the following:
-# listFilters(ensembl) # browse filter options
-# attributes = listAttributes(ensembl) # values we are interested in to retrieve
-# attributePages(ensembl) # browse attributes categories
-# attributes[attributes$page == 'sequences',] # browse attributes for sequences
-mRNA.ids <- getBM( # gene ids to all protein coding genes in celegans biomart
-  attributes = c("ensembl_gene_id","external_gene_name","go_id"), 
-  filters = "biotype", # focus on protein-coding genes as only these are identified by CEL-seq
-  values = "protein_coding", 
-  mart = ensembl)
-
-save(ensembl,mRNA.ids, file = "data/celegans-biomart-database-mRNAs.Rdata")
-
+if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
 
 # Obtain promoters of C. elegans genes-----------------------------------------
-
-#+ Load bioconductor and packages----------------------------------------------
-source("https://bioconductor.org/biocLite.R")
-if (!require(tidyverse)) {install.packages("tidyverse"); library(tidyverse)}
-if (!require(biomaRt)) {biocLite("biomaRt"); library(biomaRt)} # Ensembl biomaRt to obtain C. elegans sequences.
-
 load("data/celegans-biomart-database-mRNAs.Rdata")
 
 # Define the promoter region, relative to transcription start-site (TSS)-------
@@ -75,5 +45,4 @@ mRNA.seqs.promoter <- merge(
 # Associate gene id to each promoter sequence
 prs <- Biostrings::DNAStringSet(x = mRNA.seqs.promoter$promoter)
 names(prs) <- mRNA.seqs.promoter$ensembl_gene_id
-save(mRNA.seqs.promoter,prs,
-     file = "data/Celegans-promoters.rdata")
+save(mRNA.seqs.promoter,prs, file = "data/Celegans-promoters.rdata")
